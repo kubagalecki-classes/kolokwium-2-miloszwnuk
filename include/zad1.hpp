2a69abefd7db3c304d7d1bf24b21c21111bd487c
miloszwnuk <01157070@pw.edu.pl>
2021-01-25 17:28:29 +0000

    1

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -5,5 +5,34 @@
 #include <ostream>
 #include <sstream>
 #include <string>
+#include <iostream>
 
 // tutaj klasa Warzywo
+
+using namespace std;
+
+
+class Warzywo
+{
+    string nazwa;
+    double cena_za_kg;
+    int kolor_warzywa;
+    inline static unsigned liczba_warzyw;
+
+public:
+    
+    Warzywo(const string& a, double b, int c) : nazwa{ a }, cena_za_kg{ b },  kolor_warzywa{ c } 
+    {
+         liczba_warzyw =  liczba_warzyw + 1;
+    }
+
+    ~Warzywo() {  liczba_warzyw =  liczba_warzyw - 1; }
+
+    unsigned getVeg() { return liczba_warzyw; }
+
+    void opis(ostream& d) const
+    {
+        cout << nazwa << ": " << cena_za_kg << ", " << kolor_warzywa << endl;
+    }
+
+};
